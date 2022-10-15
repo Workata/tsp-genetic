86cc7ce0e440246bb1349038160272f8053f806e
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-10-15 21:56:10 +0200

    Add base for genetic implementation

--- src/algorithms/base.py
+++ src/algorithms/base.py
@@ -3,6 +3,8 @@ import typing as t
 from utils import Calculator, AdjencyMatrixCreator
 from models import Vertex
 from loaders import InstanceLoader
+from models import Instance
+import random
 
 
 class BaseTspSolver(ABC):
@@ -15,3 +17,16 @@ class BaseTspSolver(ABC):
     @abstractmethod
     def solve(self) -> t.Tuple[int, t.List[Vertex]]:
         pass
+
+    def _generate_random_route(self, instance: Instance, add_last_vertex: bool = True) -> t.List[Vertex]:
+        new_route = instance.vertices.copy()
+        random.shuffle(new_route)
+        if add_last_vertex:
+            new_route.append(new_route[0])
+        return new_route
+
+    def _calculate_total_distance(self, route: t.List[Vertex]) -> int:
+        total_distance: float = 0.0
+        for i in range(len(route) - 1):
+            total_distance += self._calculator.calculate_distance_between_vertices(route[i], route[i+1])
+        return round(total_distance, 2)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from algorithms import GreedyTspSolver, RandomTspSolver
+from algorithms import GreedyTspSolver, RandomTspSolver, GeneticTspSolver
 
 
 print("Greedy TSP problem solver:")
@@ -8,3 +8,7 @@ greedy_solver.solve()
 print("Randomized TSP problem solver:")
 random_solver = RandomTspSolver()
 random_solver.solve()
+
+print("Genetic TSP problem solver:")
+genetic_solver = GeneticTspSolver()
+genetic_solver.solve()
