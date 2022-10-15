8413137ae9207a71b03c358d19aa22642f388cf5
Konrad <konrad.abramowski@azurro.pl>
2022-10-15 19:16:53 +0200

    Run greedy tsp solver for N vertices instead of first only

--- src/algorithms/greedy.py
+++ src/algorithms/greedy.py
@@ -17,41 +17,47 @@ class GreedyTspSolver(BaseTspSolver):
         instance = self._instance_loader.load("./instances/berlin11_modified.tsp")
         adj_matrix = self._matrix_creator.create(instance)
 
+        min_distance: float = np.Inf
+        min_route: t.List[Vertex] = None
 
-        starting_vertex = instance.vertices[0]
-        visited_vertices = [starting_vertex]
-        total_cost = 0
-
-        temp_mini = np.Inf
-        temp_vertex = None
-
-        while len(visited_vertices) != instance.dimension:
-            vertex_a = visited_vertices[-1]     # V_a => recently visited vertex
+        for starting_vertex in instance.vertices:
+            visited_vertices = [starting_vertex]
+            total_cost = 0
 
             temp_mini = np.Inf
             temp_vertex = None
 
-            # * find the lowest cost of V_a -----> V_b connection (edge)
-            for vertex_b in instance.vertices:
-                if vertex_b in visited_vertices:
-                    continue
+            while len(visited_vertices) != instance.dimension:
+                vertex_a = visited_vertices[-1]  # V_a => recently visited vertex
 
-                distance = adj_matrix[vertex_a.number][vertex_b.number]
-                if temp_mini > distance:
-                    temp_vertex = vertex_b
-                    mini = distance
+                temp_mini = np.Inf
+                temp_vertex = None
 
-            visited_vertices.append(temp_vertex)
-            total_cost += mini
+                # * find the lowest cost of V_a -----> V_b connection (edge)
+                for vertex_b in instance.vertices:
+                    if vertex_b in visited_vertices:
+                        continue
 
-        # ! add cost of a last edge
-        last_vertex = visited_vertices[-1]
-        total_cost += adj_matrix[last_vertex.number][starting_vertex.number]
-        # ! add starting vertex again
-        visited_vertices.append(starting_vertex)
+                    distance = adj_matrix[vertex_a.number][vertex_b.number]
+                    if temp_mini > distance:
+                        temp_vertex = vertex_b
+                        mini = distance
 
-        total_cost = round(total_cost, 2)
+                visited_vertices.append(temp_vertex)
+                total_cost += mini
 
-        print((total_cost, visited_vertices))
+            # ! add cost of a last edge
+            last_vertex = visited_vertices[-1]
+            total_cost += adj_matrix[last_vertex.number][starting_vertex.number]
+            # ! add starting vertex again
+            visited_vertices.append(starting_vertex)
 
-        return total_cost, visited_vertices
+            total_cost = round(total_cost, 2)
+
+            if total_cost < min_distance:
+                min_distance = total_cost
+                min_route = visited_vertices
+
+        print((min_distance, min_route))
+
+        return min_distance, min_route
