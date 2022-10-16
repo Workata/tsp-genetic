c53e3eed3d0a09ac971b5ece7a89f10263090400
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-10-16 22:36:38 +0200

    Code refactor

--- src/algorithms/random.py
+++ src/algorithms/random.py
@@ -1,4 +1,4 @@
-from models import Vertex
+from models import Vertex, Instance
 import numpy as np
 import typing as t
 from .base import BaseTspSolver
@@ -7,12 +7,16 @@ from utils import time_counter
 
 class RandomTspSolver(BaseTspSolver):
 
+    def __init__(self, instance: Instance, solver_config: dict):
+        super().__init__(instance, solver_config)
+        self._random_repeat = self._config.get("random_repeat", 1000000)
+
     @time_counter
     def solve(self):
         min_distance = np.Inf
         min_route: t.List[Vertex]
 
-        for _ in range(100000):
+        for _ in range(self._random_repeat):
             generated_route = self._generate_random_route(self.instance)
             total_distance = self._calculate_total_distance(generated_route)
 
@@ -20,5 +24,4 @@ class RandomTspSolver(BaseTspSolver):
                 min_distance = total_distance
                 min_route = generated_route
 
-        print((min_distance, min_route))
         return min_distance, min_route
