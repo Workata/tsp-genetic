64192813e22dd5490953351313a0ba3ee874dfb7
Jakub Tolsciuk <jakubtolsciuk@gmail.com>
2022-10-16 21:26:13 +0200

    Add repetitions

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from algorithms import GreedyTspSolver, RandomTspSolver, GeneticTspSolver
 from loaders import ConfigLoader, InstanceLoader
 
 # TODO csv output
-# TODO re run - repetitions
 
 CONFIG_FILE_PATH = './config.json'
 
@@ -11,7 +10,6 @@ config = ConfigLoader.load(CONFIG_FILE_PATH)
 instance_loader = InstanceLoader()
 instance = instance_loader.load(config.get('instance_file_path'))
 
-# print(config)
 
 SOLVERS = {
     "greedy": GreedyTspSolver(instance, solver_config=config.get('greedy')),
@@ -20,8 +18,15 @@ SOLVERS = {
 }
 
 algorithm = config.get('algorithm')
+repetitions = config.get('repetitions')
+
 solver = SOLVERS[algorithm]
-print(f"[INFO] TSP problem solver ({algorithm})")
-best_cost, best_route = solver.solve()
-print(f"[INFO] Best cost: {best_cost}")
-print(f"[INFO] Best route: {best_route}")
+
+
+for i in range(1, repetitions+1):
+    print(f"\n[INFO] ------------------- ITERATION {i} START -----------------")
+    print(f"[INFO] TSP problem solver ({algorithm})")
+    best_cost, best_route = solver.solve()
+    print(f"[INFO] Best cost: {best_cost}")
+    print(f"[INFO] Best route: {best_route}")
+    print(f"[INFO] ------------------- ITERATION {i} END -------------------\n")
